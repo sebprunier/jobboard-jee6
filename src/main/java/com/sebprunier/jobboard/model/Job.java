6615617c87b17a524f7b8229a53a85651eb1212b
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2012-09-24 12:03:42 +0200

    first mock rest service

--- src/main/java/com/sebprunier/jobboard/model/Job.java
+++ src/main/java/com/sebprunier/jobboard/model/Job.java
@@ -1,4 +1,4 @@
-package com.sebprunier.jobboard;
+package com.sebprunier.jobboard.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -6,9 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "JOB")
+@XmlRootElement
 public class Job {
 
 	@Id
