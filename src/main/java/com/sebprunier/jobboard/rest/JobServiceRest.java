48c40ef65bbd74dbbb3cf5001b3fd68a8c1226bb
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2012-09-24 14:33:24 +0200

    get job by id

--- src/main/java/com/sebprunier/jobboard/rest/JobServiceRest.java
+++ src/main/java/com/sebprunier/jobboard/rest/JobServiceRest.java
@@ -2,13 +2,14 @@ package com.sebprunier.jobboard.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.sebprunier.jobboard.model.Job;
 
-@Path("jobs")
+@Path("/job")
 public class JobServiceRest {
 
 	@GET
@@ -27,6 +28,19 @@ public class JobServiceRest {
 
 		// Return mocks
 		return Response.ok(jobs).build();
+	}
+
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getJob(@PathParam("id") String id) {
+		// Create mocks
+		Job job = new Job();
+		job.setId(Long.valueOf(id));
+		job.setTitle("This is the job number " + id);
+
+		// Return mocks
+		return Response.ok(job).build();
 	}
 
 }
